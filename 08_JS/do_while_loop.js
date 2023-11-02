c6a4553a1f8cc8d42a31eeeb0c63548408a9a4b4
tushar <swamitusharjee2018@gmail.com>
2023-11-02 11:43:58 +0530

    learn about loop

---
+++ 08_JS/do_while_loop.js
@@ -0,0 +1,26 @@
+// do {
+    
+// } while (condition);
+
+
+let i=0;
+
+do {
+    console.log(i);
+    i++;
+} while (i<5);
+
+
+
+
+
+
+
+
+
+let j=10;
+
+do {
+    console.log(j);
+    j++;
+} while (j<5);
---
+++ 08_JS/for_loop.js
@@ -0,0 +1,26 @@
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+    
+}
+console.log(`break statement`);
+
+// ! break statement 
+for (let i = 0; i < 5; i++) {
+
+    if(i==2){
+        break;
+    }
+    console.log(i);
+    
+}
+console.log(` continue statement`);
+
+// ! continue statement
+for (let i = 0; i < 5; i++) {
+
+    if(i==2){
+        continue;
+    }
+    console.log(i);
+    
+}
